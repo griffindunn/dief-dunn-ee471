c3bc8594f0ddca6d4f1e218fa15c5718ad2ea4bd
prd005@bucknell.edu <prd005@bucknell.edu@e684800c-a9c6-2971-0877-7766cd80fd99>
2012-11-10 20:02:30 +0000

    Successfully Loaded 1000 Word dictionary into Tree!!!!!!!!!!

--- src/Node.java
+++ src/Node.java
@@ -7,23 +7,25 @@
  * 
  */
 public class Node {
+	private final int N = 27;
+
 	private char letter;
 	private int count;
 	private Node[] list;
-	
+
 	/**
 	 * 
 	 */
 	public Node(char letter) {
 		this.letter = letter;
 		this.count = 0;
-		list = new Node[26];
+		list = new Node[N];
 	}
-	
+
 	public Node() {
 		this.letter = '?';
 		this.count = 0;
-		list = new Node[26];
+		list = new Node[N];
 	}
 
 	/**
@@ -31,8 +33,21 @@ public class Node {
 	 * @return a pointer to the next Node of the tree
 	 */
 	public Node addLetter(char letter){
-		int index = Character.toUpperCase(letter) - 65;
-		Node Nodeptr = this.list[index];
+		int index = 0;
+		if (letter == '\'') { // ' is a special case and stored in index 27
+			index = 26;
+		} else {
+			index = Character.toUpperCase(letter) - 65;
+		}
+		Node Nodeptr = null;
+		try {
+			Nodeptr = this.list[index];
+		}catch (Exception ex) {
+			System.err.println(letter);
+			ex.printStackTrace();
+
+			System.exit(1);
+		}
 		if (Nodeptr == null) {
 			Nodeptr = new Node(Character.toUpperCase(letter));
 		}
@@ -40,33 +55,33 @@ public class Node {
 		this.list[index] = Nodeptr;
 		return Nodeptr;
 	}
-	
+
 	public int getCount(){
 		return this.count;
 	}
-	
+
 	public void setCount(int count){
 		this.count = count;
 	}
-	
+
 	public int incCount(){
 		return ++this.count;
 	}
-	
+
 	public void SetNode(int index, Node nodeptr){
 		this.list[index] = nodeptr;
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		System.out.println(Character.toUpperCase('A') - 65);
-//		System.out.println(Character.toUpperCase('B') - 65);
-//		System.out.println(Character.toUpperCase('a') - 65);
-		
+		//		System.out.println(Character.toUpperCase('A') - 65);
+		//		System.out.println(Character.toUpperCase('B') - 65);
+		//		System.out.println(Character.toUpperCase('a') - 65);
+
 		int i = 0;
-		
+
 		System.out.println(i++);
 		System.out.println(++i);
 	}
