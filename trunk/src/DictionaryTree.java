5fc0d5e07cc08e4a4785acb336bbf244ea7fb6f5
prd005@bucknell.edu <prd005@bucknell.edu@e684800c-a9c6-2971-0877-7766cd80fd99>
2012-11-10 20:02:30 +0000

    Successfully Loaded 1000 Word dictionary into Tree!!!!!!!!!!

--- trunk/src/DictionaryTree.java
+++ trunk/src/DictionaryTree.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -23,18 +28,34 @@ public class DictionaryTree {
 		}
 		root.incCount();
 	}
-	
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		DictionaryTree root = new DictionaryTree();
-		root.addWord("the");
-		root.addWord("these");
-		root.addWord("this");
-		root.addWord("two");
-		root.addWord("apple");
-		System.out.println("Break");
+		Scanner scan = null;
+		try {
+			scan = new Scanner(new File("./1-1000.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.err.println("Nueman says: You didn't say the magic word");
+			System.exit(1);
+		}
+
+		while(true){
+			try {
+				root.addWord(scan.nextLine());												
+			} catch (NoSuchElementException ex) {
+				break;
+			}
+		}
+		//		root.addWord("the");
+		//		root.addWord("these");
+		//		root.addWord("this");
+		//		root.addWord("two");
+		//		root.addWord("apple");
+		System.out.println("Done");
 	}
 
 }
