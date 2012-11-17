43397ba38caa5fc6ecce7f30876a2c686a69d912
prd005@bucknell.edu <prd005@bucknell.edu@e684800c-a9c6-2971-0877-7766cd80fd99>
2012-11-17 20:14:59 +0000

    Commented current Node.java, Dictionary.Java, and LetterProbStruct.java files

--- trunk/src/LetterProbStruct.java
+++ trunk/src/LetterProbStruct.java
@@ -1,44 +1,83 @@
 /**
- * 
+ * Bucknell University ELEC 471
+ * Philip Diefenderfer and Griffin Dunn
+ * Probabilistic Typing on Touch Input Devices
  */
 
 /**
+ * Class to hold a letter and its probability of being selected next. Implements
+ * Comparable to allow for sorting to be done by Arrays class for easier
+ * programming
+ * 
  * @author prd005
- *
  */
-public class LetterProbStruct implements Comparable<LetterProbStruct>{
+public class LetterProbStruct implements Comparable<LetterProbStruct> {
 
+	/**
+	 * The Object's letter that the probability corresponds to
+	 */
 	private char letter;
+	/**
+	 * The probability of the letter being selected next
+	 */
 	private float prob;
 
-	public LetterProbStruct(char letter, float prob){
+	/**
+	 * Creates an object containing the letter and its probability
+	 * 
+	 * @param letter
+	 *            the letter to be stored
+	 * @param prob
+	 *            the letter's probability
+	 */
+	public LetterProbStruct(char letter, float prob) {
 		this.letter = letter;
 		this.prob = prob;
 	}
 
+	/**
+	 * Gets the letter sorted in the class
+	 * 
+	 * @return the letter sorted in the class
+	 */
 	public char getLetter() {
 		return letter;
 	}
-	public void setLetter(char letter) {
-		this.letter = letter;
-	}
+
+	/**
+	 * Gets the letter's probability
+	 * 
+	 * @return the probability of the letter stored in the class
+	 */
 	public float getProb() {
 		return prob;
 	}
-	public void setProb(float prob) {
-		this.prob = prob;
-	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	/**
+	 * Compares the Object containing a letter to another parameter object
+	 * 
+	 * @param o
+	 *            the object to compare to <bold>this</bold>
+	 * @return 0 if the objects are the same, -1 if <bold>this</bold> has a
+	 *         higher probability (or letter in probability draw condition) and
+	 *         1 if <bold>parameter</bold> has a higher probability (or letter
+	 *         in a draw condition).
+	 */
 	@Override
 	public int compareTo(LetterProbStruct o) {
-		if(o.getClass() == this.getClass()){
-			if (this.getProb() > o.getProb()){
+		if (o.getClass() == this.getClass()) {
+			if (this.getProb() > o.getProb()) {
 				return -1;
-			} else if (this.getProb() < o.getProb()){
+			} else if (this.getProb() < o.getProb()) {
 				return 1;
-			} else if (this.getLetter() < o.getLetter()){
+			} else if (this.getLetter() < o.getLetter()) {
 				return -1;
-			} else if (this.getLetter() > o.getLetter()){
+			} else if (this.getLetter() > o.getLetter()) {
 				return 1;
 			}
 		}
--- trunk/src/Node.java
+++ trunk/src/Node.java
@@ -1,20 +1,40 @@
 /**
- * 
+ * Bucknell University ELEC 471
+ * Philip Diefenderfer and Griffin Dunn
+ * Probabilistic Typing on Touch Input Devices
  */
 
 /**
+ * Node class for the dictionary tree containing the letter stored at this node,
+ * the number of letters stored at this node, and an array of Nodes pointed to
+ * by this node containing subsequent letters.
+ * 
  * @author prd005
  * 
  */
 public class Node {
+	/**
+	 * The number of characters to be stored (A-Z and ')
+	 */
 	private final int N = 27;
-
+	/**
+	 * The letter sorted at this node
+	 */
 	private char letter;
+	/**
+	 * The number of letters that use this node
+	 */
 	private int count;
+	/**
+	 * The Array of pointers to subsequent letters
+	 */
 	private Node[] list;
 
 	/**
+	 * Creates a new node containing the parameter letter
 	 * 
+	 * @param letter
+	 *            the letter stored at this node
 	 */
 	public Node(char letter) {
 		this.letter = letter;
@@ -22,28 +42,37 @@ public class Node {
 		list = new Node[N];
 	}
 
+	/**
+	 * Creates a root Node
+	 */
 	public Node() {
 		this.letter = '?';
 		this.count = 0;
 		list = new Node[N];
 	}
-	
+
 	/**
-	 * Copy constructor
+	 * Copy constructor to create a new Node object with the same properties as
+	 * the parameter Node
 	 * 
 	 * @param n
+	 *            the node to be copied
 	 */
-	public Node(Node n){
+	public Node(Node n) {
 		this.letter = n.letter;
 		this.count = n.count;
 		this.list = n.list;
 	}
 
 	/**
+	 * Adds a letter to the tree at the current Node. Increments the count if a
+	 * node for the new letter exists otherwise creates a new node
+	 * 
 	 * @param letter
-	 * @return a pointer to the next Node of the tree
+	 *            the letter to be added to the tree at this node
+	 * @return a pointer to the next Node
 	 */
-	public Node addLetter(char letter){
+	public Node addLetter(char letter) {
 		int index = 0;
 		if (letter == '\'') { // ' is a special case and stored in index 27
 			index = 26;
@@ -51,73 +80,118 @@ public class Node {
 			index = Character.toUpperCase(letter) - 65;
 		}
 		Node Nodeptr = null;
+
+		// why is this a try?
+		// TODO prd005
 		try {
 			Nodeptr = this.list[index];
-		}catch (Exception ex) {
+		} catch (Exception ex) {
 			System.err.println(letter);
 			ex.printStackTrace();
-
 			System.exit(1);
 		}
 		if (Nodeptr == null) {
 			Nodeptr = new Node(Character.toUpperCase(letter));
 		}
-		Nodeptr.count ++;	
+		Nodeptr.count++;
 		this.list[index] = Nodeptr;
 		return Nodeptr;
 	}
 
-	public int getCount(){
+	/**
+	 * Gets the number of words that use this node
+	 * 
+	 * @return the number of words that used this Node
+	 */
+	public int getCount() {
 		return this.count;
 	}
 
-	public void setCount(int count){
+	/**
+	 * Sets the count at the Node
+	 * 
+	 * @param count
+	 *            the count to be set
+	 */
+	public void setCount(int count) {
 		this.count = count;
 	}
 
-	public int incCount(){
+	/**
+	 * Increments the count at the Node
+	 * 
+	 * @return the count at the node
+	 */
+	public int incCount() {
 		return ++this.count;
 	}
-	
-	public int getN(){
+
+	/**
+	 * Gets the number of possible letters
+	 * 
+	 * @return the value of N
+	 */
+	public int getN() {
 		return this.N;
 	}
 
-	public void SetNode(int index, Node nodeptr){
+	/**
+	 * Sets the current Node's Array of Nodes at parameter index to the
+	 * parameter Node
+	 * 
+	 * @param index
+	 *            the index to store the parameter node at
+	 * @param nodeptr
+	 *            the node to be stored
+	 */
+	public void SetNode(int index, Node nodeptr) {
 		this.list[index] = nodeptr;
 	}
 
-	public float[] getProbs(){
+	/**
+	 * Gets an Array of probabilities for the subsequent letters
+	 * 
+	 * @return the probabilities for each character of the tree
+	 */
+	public float[] getProbs() {
 		float probs[] = new float[N];
-		
-		for (int i = 0; i < N; i++){
-			if (list[i] == null){
+
+		for (int i = 0; i < N; i++) {
+			if (list[i] == null) {
 				probs[i] = 0;
 			} else {
-				probs[i] = (float) list[i].count/ this.count;	
+				probs[i] = (float) list[i].count / this.count;
 			}
 		}
-		
+
 		return probs;
 	}
-	
-	public Node getNode(char letter){
+
+	/**
+	 * Gets the Node that corresponds to the parameter letter
+	 * 
+	 * @param letter
+	 *            the letter to return the Node for
+	 * @return the Node for the parameter letter
+	 */
+	public Node getNode(char letter) {
 		int index = Character.toUpperCase(letter) - 65;
-		if (index < 0){
+		if (index < 0) {
 			index = 26;
 		}
 		return this.list[index];
 	}
-	
+
 	/**
 	 * Scratch pad for testing stuff
 	 * 
-	 * @param args unused
+	 * @param args
+	 *            unused
 	 */
 	public static void main(String[] args) {
-		//		System.out.println(Character.toUpperCase('A') - 65);
-		//		System.out.println(Character.toUpperCase('B') - 65);
-		//		System.out.println(Character.toUpperCase('a') - 65);
+		// System.out.println(Character.toUpperCase('A') - 65);
+		// System.out.println(Character.toUpperCase('B') - 65);
+		// System.out.println(Character.toUpperCase('a') - 65);
 
 		int i = 0;
 
